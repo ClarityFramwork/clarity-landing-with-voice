34cf31c1e3dcd54cae45d8a917beb60957ff4d90
ClarityFramwork <brandond.francis@icloud.com>
2025-08-13 12:21:51 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
